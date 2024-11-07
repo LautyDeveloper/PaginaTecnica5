d30713dcfeb1317a61601542c6c50546e0b7d9a0
Lautaro Aquino <laufabio24@gmail.com>
2024-11-07 13:11:59 -0300

    Añadimos seccion de Noticias para Profesores

--- src/routes/profesores.routes.js
+++ src/routes/profesores.routes.js
@@ -5,7 +5,12 @@ const router = Router();
 
 // Rutas existentes
 router.get("/profesores", async (req, res) => {
-  res.render("profesores/profesores");
+  try {
+    const [result] = await pool.query("SELECT * FROM noticiasprofesores");
+    res.render("profesores/profesores", { noticias: result });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 export default router;
